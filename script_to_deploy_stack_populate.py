051e2b2c5f22e02038ddd2ea746c8b8153cc1e37
Zhiwei Ding <zhiweid@bcm.edu>
2022-05-14 22:36:22 -0500

    manually populate stack tables for closed loop 27424-7-11

--- script_to_deploy_stack_populate.py
+++ script_to_deploy_stack_populate.py
@@ -1,3 +1,11 @@
+from pipeline import stack
+key = {'animal_id': 27424, 'session': 7, 'stack_idx': 11}
+stack.Quality.populate(key, reserve_jobs=True, suppress_errors=True)
+stack.RasterCorrection.populate(key, reserve_jobs=True, suppress_errors=True)
+stack.MotionCorrection.populate(key, reserve_jobs=True, suppress_errors=True)
+stack.Stitching.populate(key, reserve_jobs=True, suppress_errors=True)
+stack.CorrectedStack.populate(key, reserve_jobs=True, suppress_errors=True)
+
 # from pipeline import stack
 
 # stack_key = [{'animal_id': 26897, 'stack_session': 2, 'stack_idx': 1},
@@ -6,11 +14,11 @@
 #             ]
 # stack.Segmentation.populate(stack_key, reserve_jobs=True)
 
-from pipeline import stack
-import datajoint as dj
-data_schemas = dj.create_virtual_module('neurodata_static', 'neurodata_static')
-analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
+# from pipeline import stack
+# import datajoint as dj
+# data_schemas = dj.create_virtual_module('neurodata_static', 'neurodata_static')
+# analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
 
-mei_keys = stack.Registration * analyses.DEIClosedLoopSummaryResults.proj(scan_session = 'session') & 'scan_session = stack_session'
-imagenet_keys = stack.Registration * (data_schemas.StaticMultiDatasetGroupAssignment() & 'group_id in (233, 237, 239, 241, 243)').proj(scan_session = 'session') & 'scan_session = stack_session'
-stack.RegistrationOverTime.populate([mei_keys, imagenet_keys], reserve_jobs=True, order='random')
+# mei_keys = stack.Registration * analyses.DEIClosedLoopSummaryResults.proj(scan_session = 'session') & 'scan_session = stack_session'
+# imagenet_keys = stack.Registration * (data_schemas.StaticMultiDatasetGroupAssignment() & 'group_id in (233, 237, 239, 241, 243)').proj(scan_session = 'session') & 'scan_session = stack_session'
+# stack.RegistrationOverTime.populate([mei_keys, imagenet_keys], reserve_jobs=True, order='random')
