1b83a5e7d2601267aacbfea8e803c6e58fb36087
Zhiwei Ding <zhiweid@bcm.edu>
2022-09-06 13:07:01 -0500

    populate RegistrationOverTime for new closed loop scans

--- script_to_deploy_stack_populate.py
+++ script_to_deploy_stack_populate.py
@@ -3,7 +3,7 @@ import datajoint as dj
 data_schemas = dj.create_virtual_module('neurodata_static', 'neurodata_static')
 analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
 
-groups = [233, 237, 239, 243, 271, 272, 273, 275, 279]
+groups = [233, 237, 239, 243, 271, 272, 273, 275, 279, 284, 285]
 mei_keys = stack.Registration * analyses.DEIClosedLoopSummaryResults.proj(scan_session = 'session') & 'scan_session = stack_session'
 imagenet_keys = stack.Registration * (data_schemas.StaticMultiDatasetGroupAssignment() & [{'group_id':g} for g in groups]).proj(scan_session = 'session') & 'scan_session = stack_session'
 stack.RegistrationOverTime.populate([mei_keys, imagenet_keys], reserve_jobs=True, order='random')
