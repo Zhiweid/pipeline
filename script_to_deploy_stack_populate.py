5b095c6d474a3ed469f39e43439fd7329cff9eaa
Zhiwei Ding <zhiweid@bcm.edu>
2023-04-10 16:11:51 -0500

    populate stack registration over time for closed loop group 329 and 330 scans

--- script_to_deploy_stack_populate.py
+++ script_to_deploy_stack_populate.py
@@ -1,12 +1,15 @@
 from pipeline import stack
 import datajoint as dj
-data_schemas = dj.create_virtual_module('neurodata_static', 'neurodata_static')
-analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
+# data_schemas = dj.create_virtual_module('neurodata_static', 'neurodata_static')
+# analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
 
-groups = [233, 237, 239, 243, 271, 272, 273, 275, 279, 284, 285]
-mei_keys = stack.Registration * analyses.DEIClosedLoopSummaryResults.proj(scan_session = 'session') & 'scan_session = stack_session'
-imagenet_keys = stack.Registration * (data_schemas.StaticMultiDatasetGroupAssignment() & [{'group_id':g} for g in groups]).proj(scan_session = 'session') & 'scan_session = stack_session'
-stack.RegistrationOverTime.populate([mei_keys, imagenet_keys], reserve_jobs=True, order='random')
+# groups = [233, 237, 239, 243, 271, 272, 273, 275, 279, 284, 285]
+# mei_keys = stack.Registration * analyses.DEIClosedLoopSummaryResults.proj(scan_session = 'session') & 'scan_session = stack_session'
+# imagenet_keys = stack.Registration * (data_schemas.StaticMultiDatasetGroupAssignment() & [{'group_id':g} for g in groups]).proj(scan_session = 'session') & 'scan_session = stack_session'
+# stack.RegistrationOverTime.populate([mei_keys, imagenet_keys], reserve_jobs=True, order='random')
+
+keys = (meso.ScanDone & (collection.CuratedScan & 'study_name LIKE "%%dei_v1%%" and animal_id = 29513 and scan_purpose in ("imagenet", "dei_texture_package")')).fetch(as_dict=True)
+stack.RegistrationOverTime.populate(keys, reserve_jobs=True, order='random')
 
 # stack_keys = [{'animal_id': 27468, 'stack_session': 13, 'stack_idx': 19},
 #               {'animal_id': 27468, 'stack_session': 14, 'stack_idx': 11},
