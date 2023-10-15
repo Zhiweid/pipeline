af1fb3130bd6c03c61241d089fb8fcf97e1ec8e1
Zhiwei Ding <zhiweid@bcm.edu>
2023-10-15 14:45:25 -0500

    registration over time for dynamic static closed loop scans

--- script_to_deploy_stack_populate.py
+++ script_to_deploy_stack_populate.py
@@ -4,7 +4,8 @@ import datajoint as dj
 # analyses = dj.create_virtual_module('neurostatic_pilot_analyses', 'neurostatic_pilot_analyses')
 collection = dj.create_virtual_module('pipeline_collection', 'pipeline_collection')
 
-keys = (collection.CuratedScan & 'study_name LIKE "%%dei_v1%%" and animal_id in (29513, 29515) and scan_purpose in ("imagenet", "dei_texture_package")').proj(scan_session='session')
+# keys = (collection.CuratedScan & 'study_name LIKE "%%dei_v1%%" and animal_id in (29513, 29515) and scan_purpose in ("imagenet", "dei_texture_package")').proj(scan_session='session')
+keys = (collection.CuratedScan & 'study_name LIKE "%%dynamic_static_deis_closed_loop%%" and animal_id in (30417) and scan_purpose in ("platinum_with_static_oracle", "static_images_with_dynamic_oracle", "dynamic_static_validation_deis")').proj(scan_session='session')
 keys = stack.Registration & keys & 'scan_session = stack_session'
 stack.RegistrationOverTime.populate(keys, reserve_jobs=True, order='random')
 
