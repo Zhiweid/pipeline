4877ebec152d7ac99f457f1090dde5c04c73049b
Zhiwei Ding <zhiweid@bcm.edu>
2024-01-29 15:18:50 -0600

    manually populate Tracking

--- script_to_deploy_pupil.py
+++ script_to_deploy_pupil.py
@@ -1,4 +1,4 @@
 from pipeline import pupil
-key = dict(animal_id=26645, session=2, scan_idx=18)
+key = dict(animal_id=29189, session=16, scan_idx=1)
 pupil.Tracking.populate(key, 'tracking_method = 2', reserve_jobs=True)
 pupil.FittedPupil.populate(key, reserve_jobs=True)
